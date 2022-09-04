cb2babc39de546151f9d729cec06c1caa853840f
KOBOTONG <Merrsthp@gmail.com>
2022-09-05 02:18:44 +0700

    edit0509

--- lib/states/create_acc.dart
+++ lib/states/create_acc.dart
@@ -1,18 +1,216 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:demosafespace/utility/constant.dart';
+
 class CreareAcc extends StatefulWidget {
-  const CreareAcc({ Key? key }) : super(key: key);
+  const CreareAcc({Key? key}) : super(key: key);
 
   @override
   State<CreareAcc> createState() => _CreareAccState();
 }
 
 class _CreareAccState extends State<CreareAcc> {
+  bool statusRedEye = true;
+  get childen => null;
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
-      
+      backgroundColor: Constant.yello,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            makeTitalStart(),
+            makeTitleC(),
+            firstname(size),
+            lastname(size),
+            email(size),
+            password(size),
+            Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 11, bottom: 10),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Constant().h3Style(),
+              labelText: 'Phone Number',
+              prefixIcon: Icon(
+                Icons.phone_android_outlined,
+                color: Constant.ligthBlack,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Constant.ligthBlack),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Constant.ligthBlack),
+              ),
+            ),
+          ),
+        ),
+      ],
+    )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row password(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 15, bottom: 10),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye
+                    ? Icon(Icons.remove_red_eye, color: Constant.ligthBlack)
+                    : Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: Constant.colorF,
+                      ),
+              ),
+              labelStyle: Constant().h3Style(),
+              labelText: 'Password :',
+              prefixIcon: Icon(
+                Icons.lock_outline,
+                color: Constant.ligthBlack,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Constant.ligthBlack),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Constant.ligthBlack,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row email(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 11, bottom: 10),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Constant().h3Style(),
+              labelText: 'E-mail',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: Constant.ligthBlack,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Constant.ligthBlack),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Constant.ligthBlack),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row lastname(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Constant().h3Style(),
+              labelText: 'Lastname',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: Constant.ligthBlack,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Constant.ligthBlack),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Constant.ligthBlack),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row firstname(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 30, bottom: 10),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Constant().h3Style(),
+              labelText: 'Firstname',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: Constant.ligthBlack,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Constant.ligthBlack),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Constant.ligthBlack),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container makeTitleC() {
+    return Container(
+      padding: EdgeInsets.only(left: 20, top: 10),
+      child: Row(
+        children: [
+          Text(
+            "Create tour accound for join application",
+            style: Constant().hStyle(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container makeTitalStart() {
+    return Container(
+      padding: EdgeInsets.only(left: 18, top: 50),
+      child: Row(
+        children: [
+          Text(
+            "Get Started !",
+            style: Constant().h1Style(),
+          ),
+        ],
+      ),
     );
   }
 }
